a819abc59b13c46fa86f27292d9511741cd5d334
Jeremiah VALERIE <jeremiah@overblog.com>
2019-03-24 16:34:02 +0100

    Init

---
+++ config/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+];
---
+++ src/GraphQL/Resolver/SubscriptionResolverMap.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\GraphQL\Resolver;
+
+use Overblog\GraphQLBundle\Resolver\ResolverMap;
+use Overblog\GraphQLBundle\Subscription\RealtimeNotifier;
+
+class SubscriptionResolverMap extends ResolverMap
+{
+    private $realTimeNotifier;
+
+    private $rooms = [
+        'foo' => ['id' => 1, 'name' => 'foo', 'messages' => []],
+        'bar' => ['id' => 2, 'name' => 'bar', 'messages' => []],
+    ];
+
+    public function __construct(RealtimeNotifier $realTimeNotifier)
+    {
+        $this->realTimeNotifier = $realTimeNotifier;
+    }
+
+    protected function map()
+    {
+        $roomByName = function ($name) {
+            return $this->rooms[$name] ?? null;
+        };
+
+        $findMessagesForRoom = function ($room) {
+            return $this->rooms[$room['name']]['messages'] ?? [];
+        };
+
+        $roomType = [
+            'messages' => $findMessagesForRoom,
+        ];
+
+        $queryType = [
+            'rooms' => function () {
+                return $this->rooms;
+            },
+            'messages' => function ($value, $args) use ($roomByName, $findMessagesForRoom) {
+                $roomName = $args['roomName'];
+                $room = $roomByName($roomName);
+                $messages = $findMessagesForRoom($room);
+
+                return $messages;
+            },
+        ];
+
+        $mutationType = [
+            'chat' => function ($value, $args) use ($roomByName) {
+                $roomName = $args['roomName'];
+                $body = $args['body'];
+
+                $room = $roomByName($roomName);
+
+                $message = [];
+                $message['roomId'] = $room['id'];
+                $message['body'] = $body;
+                $message['timestamp'] = (new \DateTime())->format(\DateTime::ATOM);
+
+                $this->rooms[$roomName]['messages'][] = $message;
+
+                // Then we can publish new messages that arrives from the chat mutation
+                $this->realTimeNotifier->notify('inbox', $message); // <- Exactly what "inbox" will receive
+
+                return $message;
+            },
+        ];
+
+        $subscriptionType = [
+            'inbox' => function ($message) {
+                return $message;
+            },
+        ];
+
+        return [
+            'Room' => $roomType,
+            'Query' => $queryType,
+            'Mutation' => $mutationType,
+            'Subscriber' => $subscriptionType,
+        ];
+    }
+}
